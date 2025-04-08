d29814a48f4a522a89a9e7f7d53adfb366644f76
Zackary Scholl <zack.scholl@gmail.com>
2025-04-07 20:08:41 -0700

    pause tests

--- src/player/player_test.go
+++ src/player/player_test.go
@@ -1,36 +1,25 @@
 package player
 
-import (
-	"testing"
-
-	"asdfgh/src/player/midi"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestPlayer(t *testing.T) {
-	// sudo modprobe snd-virmidi midi_devs=1
-	var p Player
-	var err error
-
-	p, err = midi.New("Raw MIDI")
-	assert.Nil(t, err)
-	assert.Nil(t, p.NoteOn(1, 60, 100))
-	assert.Nil(t, p.NoteOff(1, 60))
-	assert.Nil(t, p.NoteOn(1, 62, 100))
-	assert.Nil(t, p.NoteOn(1, 64, 100))
-	assert.Nil(t, p.Close())
-}
+// func TestPlayer(t *testing.T) {
+// 	// sudo modprobe snd-virmidi midi_devs=1
+// 	var p Player
+// 	var err error
+// 	p, err = midi.New("Raw MIDI")
+// 	assert.Nil(t, err)
+// 	assert.Nil(t, p.NoteOn(1, 60, 100))
+// 	assert.Nil(t, p.NoteOff(1, 60))
+// 	assert.Nil(t, p.NoteOn(1, 62, 100))
+// 	assert.Nil(t, p.NoteOn(1, 64, 100))
+// 	assert.Nil(t, p.Close())
+// }
 
 // func TestPlaying(t *testing.T) {
 // 	var p Player
 // 	var err error
-
 // 	p, err = midi.New("OP-Z")
 // 	if err != nil {
 // 		return
 // 	}
-
 // 	stepString := "Cmaj@u2d4!120,30"
 // 	step := step.Step{Original: stepString}
 // 	_, err = step.Parse(constants.MODIFIER_NOTE, 60)
@@ -44,7 +33,6 @@ func TestPlayer(t *testing.T) {
 // 		Gate:        0.5,
 // 	})
 // 	assert.Nil(t, err)
-
 // 	time.Sleep(3 * time.Second)
 // 	assert.Nil(t, p.Close())
 // }
