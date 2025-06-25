daa248f5e29ee578ef07e7a77b3624abebb3af23
Zack Scholl <zack.scholl@gmail.com>
2025-06-25 10:19:28 -0700

    fix: add supercollider player

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module museq
 go 1.24
 
 require (
+	github.com/fsnotify/fsnotify v1.9.0
+	github.com/hypebeast/go-osc v0.0.0-20220308234300-cec5a8a1e5f5
 	github.com/schollz/logger v1.2.0
 	github.com/stretchr/testify v1.10.0
 	gitlab.com/gomidi/midi/v2 v2.2.19
@@ -10,8 +12,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/loov/hrtime v1.0.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ src/player/supercollider/supercollider.go
@@ -0,0 +1,86 @@
+package supercollider
+
+import (
+	"fmt"
+
+	"github.com/hypebeast/go-osc/osc"
+	log "github.com/schollz/logger"
+)
+
+type Player struct {
+	Name         string
+	client       *osc.Client
+	opened       bool
+	oscPort      int
+	functionPath string
+}
+
+func New(name string, oscPort int, functionPath string) (p *Player, err error) {
+	if oscPort == 0 {
+		oscPort = 57120
+	}
+	if functionPath == "" {
+		functionPath = "/note"
+	}
+
+	p0 := Player{
+		Name:         fmt.Sprintf("supercollider-%s-%d", name, oscPort),
+		oscPort:      oscPort,
+		functionPath: functionPath,
+	}
+
+	p0.client = osc.NewClient("localhost", oscPort)
+	if p0.client == nil {
+		err = fmt.Errorf("failed to create OSC client for port %d", oscPort)
+		log.Errorf("Error creating OSC client: %s", err)
+		return
+	}
+
+	p = &p0
+	p.opened = true
+	log.Infof("opened SuperCollider OSC client on port %d with function path %s", oscPort, functionPath)
+
+	return
+}
+
+func (sc Player) String() string {
+	return sc.Name
+}
+
+func (sc *Player) Close() (err error) {
+	log.Tracef("close")
+	if sc.opened {
+		sc.opened = false
+		log.Infof("closed SuperCollider OSC client %s", sc.Name)
+	}
+	return
+}
+
+func (sc *Player) NoteOn(note int, velocity int) (err error) {
+	log.Tracef("note_on (%d,%d)\n", note, velocity)
+	if sc.opened {
+		msg := osc.NewMessage(sc.functionPath)
+		msg.Append("noteOn")
+		msg.Append(int32(note))
+		msg.Append(int32(velocity))
+		err = sc.client.Send(msg)
+		if err != nil {
+			log.Errorf("Error sending OSC note_on message: %s", err)
+		}
+	}
+	return
+}
+
+func (sc *Player) NoteOff(note int) (err error) {
+	log.Tracef("note_off (%d)", note)
+	if sc.opened {
+		msg := osc.NewMessage(sc.functionPath)
+		msg.Append("noteOff")
+		msg.Append(int32(note))
+		err = sc.client.Send(msg)
+		if err != nil {
+			log.Errorf("Error sending OSC note_off message: %s", err)
+		}
+	}
+	return
+}
